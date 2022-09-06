1015b9ac79d9d93658ac942c5ff429a988b05cf2
pratik1100101 <111274116+pratik1100101@users.noreply.github.com>
2022-09-06 16:51:34 +0530

    Add files via upload

---
+++ simpleCalculatorIndex.mjs
@@ -0,0 +1,5 @@
+import * as calculator from './simpleCalculator.mjs';
+console.log(calculator.add(22,67));
+console.log(calculator.subtract(40,23));
+console.log(calculator.divide(35,7));
+console.log(calculator.multiply(2,6));
